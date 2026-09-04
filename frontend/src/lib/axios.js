e52592fa81bdfb94e5110a03fa204f8c66cf4fd6
Joao-Enrique-al <sudoaptgetinstalljoao93@gmail.com>
2026-09-04 15:13:24 -0300

    Fix: webservices(axios)

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-dwwo.onrender.com" : "/api",
+  baseURL: "https://chat-dwwo.onrender.com/api",
   withCredentials: true,
 });
